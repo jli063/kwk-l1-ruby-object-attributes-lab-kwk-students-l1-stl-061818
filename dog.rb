34696247946f85a8b340056c6cf1702f5f7dc21b
jli063 <jli063@rsdmo.org>
2018-06-22 17:40:49 +0000

    automatically backed up by learn

--- dog.rb
+++ dog.rb
@@ -1,11 +1,13 @@
 class Dog
-  @name=name
-  def name=this_is_dog_name
-    this_is_dog_name=name
+  def name=(dogs_name)
+    @this_dogs_name=dogs_name
   end
+  
   def name 
-    puts "#{name}"
+    @this_dogs_name
   end
 end
 fido = Dog.new
-fido.name="Fido"
+fido.name= "Fido"
+
+puts fido.name
