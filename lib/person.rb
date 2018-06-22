4a764050116476da2234150c72b23ae8a60dbe68
jli063 <jli063@rsdmo.org>
2018-06-22 17:48:38 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,27 @@
 # person.rb
+class Person
+ 
+  def name=(person_name)
+    @this_person_name = person_name
+  end
+ 
+  def name
+    @this_person_name
+  end
+  
+  def job=(person_job)
+    @this_person_job = person_job
+  end
+  
+  def job
+    @this_person_job
+  end
+  
+end
+ 
+beyonce = Person.new
+beyonce.name = "Beyonce"
+beyonce.job = "Singer"
+ 
+puts beyonce.name
+puts beyonce.job 
